1e8c503d705008b16931561f646755f9afc7010f
Matteus Hemström <matteus92@gmail.com>
2021-11-01 21:17:34 +0100

    add base travel time of 3 fields

--- internal/travel.go
+++ internal/travel.go
@@ -8,9 +8,10 @@ import (
 // Calculates the arrival time from now.
 // Returns the travel time in nanoseconds.
 func CalculateArrivalTime(fromX, fromY, toX, toY int32, speed time.Duration) int64 {
+	base := 3.0
 	dx := toX - fromX
 	dy := toY - fromY
-	distance := math.Sqrt(float64(dx*dx) + float64(dy*dy))
+	distance := math.Sqrt(float64(dx*dx) + float64(dy*dy)) + base
 	travelTime := distance * speed.Seconds()
 	return time.Now().UnixNano() + int64(travelTime*1e9)
 }
