48472f316da89e09277a5949534af756425206f9
Matteus Hemström <matteus92@gmail.com>
2022-08-31 20:43:48 +0200

    webapp: small screen improvements

--- webapp/src/Game/buildings/ResearchInstitute.tsx
+++ webapp/src/Game/buildings/ResearchInstitute.tsx
@@ -2,12 +2,8 @@ import React, { useState } from "react";
 import classnames from "classnames";
 import { Link, Route, Routes, useNavigate, useParams } from "react-router-dom";
 import { useStore } from "../../store";
-import styles, { button, primaryButton } from "../../styles";
-import {
-  formatDurationShort,
-  formatNanoTimestampToNowShort,
-  formatNumber,
-} from "../../utils";
+import { button, primaryButton } from "../../styles";
+import { formatNanoTimestampToNowShort, formatNumber } from "../../utils";
 import { ReactComponent as SvgArrowRight } from "images/icons/arrow-right.svg";
 import {
   ResearchDiscovery,
@@ -15,253 +11,10 @@ import {
   ResearchTree,
 } from "../../generated/research";
 import { ReactComponent as SvgResearchInstitute } from "../../../images/research-institute.svg";
-import { ReactComponent as SvgWebsite } from "../../../images/research/website.svg";
-import { ReactComponent as SvgDigitalOrderingSystem } from "../../../images/research/digital-ordering-system.svg";
-import { ReactComponent as SvgMobileApp } from "../../../images/research/mobile-app.svg";
-import { ReactComponent as SvgMasoneryOven } from "../../../images/research/masonery-oven.svg";
-import { ReactComponent as SvgGasOven } from "../../../images/research/gas-oven.svg";
-import { ReactComponent as SvgHybridOven } from "../../../images/research/hybrid-oven.svg";
-import { ReactComponent as SvgDurumWheat } from "../../../images/research/durum-wheat.svg";
-import { ReactComponent as SvgDoubleZeroFlour } from "../../../images/research/double-zero-flour.svg";
-import { ReactComponent as SvgSanMaraznoTomatoes } from "../../../images/research/san-marzano-tomatoes.svg";
-import { ReactComponent as SvgOcimumBasilicum } from "../../../images/research/ocimum-basilicum.svg";
-import { ReactComponent as SvgExtraVirgin } from "../../../images/research/extra-virgin.svg";
-import { ReactComponent as SvgCheck } from "../../../images/icons/check.svg";
 import { uniq } from "lodash";
 import { Coin, GeniusFlash, Pizza } from "../../icons";
 import ReactMarkdown from "react-markdown";
 
-const title = classnames("text-lg", "md:text-xl", "mb-2");
-const label = classnames("text-xs", "md:text-sm");
-const value = classnames("text-sm");
-const descriptionStyle = classnames("text-sm", "text-gray-800");
-
-const PlaceholderImage: React.VFC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="50"
-    height="50"
-    viewBox="0 0 50 50"
-    {...props}
-  >
-    <rect fill="#ddd" width="50" height="50" />
-    <text
-      fill="rgba(0,0,0,0.5)"
-      fontFamily="sans-serif"
-      fontSize="10"
-      dy="10.5"
-      fontWeight="bold"
-      x="50%"
-      y="50%"
-      textAnchor="middle"
-    >
-      50x50
-    </text>
-  </svg>
-);
-
-const svgs: Record<ResearchDiscovery, React.VFC | undefined> = {
-  [ResearchDiscovery.WEBSITE]: SvgWebsite,
-  [ResearchDiscovery.DIGITAL_ORDERING_SYSTEM]: SvgDigitalOrderingSystem,
-  [ResearchDiscovery.MOBILE_APP]: SvgMobileApp,
-  [ResearchDiscovery.MASONRY_OVEN]: SvgMasoneryOven,
-  [ResearchDiscovery.GAS_OVEN]: SvgGasOven,
-  [ResearchDiscovery.HYBRID_OVEN]: SvgHybridOven,
-  [ResearchDiscovery.DURUM_WHEAT]: SvgDurumWheat,
-  [ResearchDiscovery.DOUBLE_ZERO_FLOUR]: SvgDoubleZeroFlour,
-  [ResearchDiscovery.SAN_MARZANO_TOMATOES]: SvgSanMaraznoTomatoes,
-  [ResearchDiscovery.OCIMUM_BASILICUM]: SvgOcimumBasilicum,
-  [ResearchDiscovery.EXTRA_VIRGIN]: SvgExtraVirgin,
-
-  [ResearchDiscovery.SLAM]: PlaceholderImage,
-  [ResearchDiscovery.CARDIO]: PlaceholderImage,
-  [ResearchDiscovery.COFFEE]: PlaceholderImage,
-  [ResearchDiscovery.HIT_IT]: PlaceholderImage,
-  [ResearchDiscovery.TIP_TOE]: PlaceholderImage,
-  [ResearchDiscovery.ON_A_ROLL]: PlaceholderImage,
-  [ResearchDiscovery.TRIP_WIRE]: PlaceholderImage,
-  [ResearchDiscovery.GODS_TOUCH]: PlaceholderImage,
-  [ResearchDiscovery.GRAND_SLAM]: PlaceholderImage,
-  [ResearchDiscovery.WHITEBOARD]: PlaceholderImage,
-  [ResearchDiscovery.BIG_POCKETS]: PlaceholderImage,
-  [ResearchDiscovery.CONSECUTIVE]: PlaceholderImage,
-  [ResearchDiscovery.LASER_ALARM]: PlaceholderImage,
-  [ResearchDiscovery.NIGHTS_WATCH]: PlaceholderImage,
-  [ResearchDiscovery.SHADOW_EXPERT]: PlaceholderImage,
-  [ResearchDiscovery.STRESS_HANDLING]: PlaceholderImage,
-  [ResearchDiscovery.BOOTS_OF_HASTE]: PlaceholderImage,
-  [ResearchDiscovery.KITCHEN_STRATEGY]: PlaceholderImage,
-  [ResearchDiscovery.THIEVES_FAVORITE_BAG]: PlaceholderImage,
-};
-
-const numberFormat = new Intl.NumberFormat();
-
-const ResearchNodeView: React.VFC<{
-  node: ResearchInfo;
-  discovered: boolean;
-  parentDiscovered: boolean;
-  opened: boolean;
-  onToggleOpen: () => void;
-}> = ({ node, discovered, opened, onToggleOpen, parentDiscovered }) => {
-  const geniusFlashes = useStore((state) => state.gameState.geniusFlashes ?? 0);
-  const startResearch = useStore((state) => state.startResearch);
-  const researchQueue = useStore((state) => state.gameState.researchQueue);
-
-  const onSubmit = () => {
-    startResearch(node.discovery);
-    window.scroll(0, 0);
-  };
-
-  const SvgImage = svgs[node.discovery] || PlaceholderImage;
-
-  const isBeingResearched = researchQueue.some(
-    (x) => x.discovery === node.discovery
-  );
-
-  const canAfford = geniusFlashes > 0;
-  const disabled = !canAfford;
-  const showResearchButton =
-    !isBeingResearched && !discovered && parentDiscovered;
-
-  if (!opened) {
-    return (
-      <div className={classnames("self-center", "flex", "relative")}>
-        <button
-          className={classnames(
-            "flex",
-            "w-16",
-            "h-16",
-            "border-2",
-            "rounded-md",
-            "border-black",
-            "bg-yellow-50",
-            "p-1"
-          )}
-          onClick={() => onToggleOpen()}
-          aria-expanded={opened}
-          data-cy="research-node-expand-toggle"
-        >
-          <SvgImage className={classnames("w-full", "h-full")} />
-        </button>
-        {discovered && (
-          <SvgCheck
-            className={classnames(
-              "w-8",
-              "h-8",
-              "absolute",
-              "-right-12",
-              "top-4"
-            )}
-          />
-        )}
-      </div>
-    );
-  }
-
-  return (
-    <div
-      className={classnames(
-        "flex",
-        "flex-col",
-        "items-center",
-        "mb-8",
-        "gap-6",
-        "lg:gap-8",
-        "bg-green-100",
-        "p-2"
-      )}
-    >
-      <button
-        className={classnames(
-          "flex-shrink-0",
-          "w-24",
-          "h-24",
-          "bg-yellow-100",
-          "border-4",
-          "rounded-md",
-          "border-black",
-          "p-1"
-        )}
-        onClick={() => onToggleOpen()}
-        aria-expanded={opened}
-        data-cy="research-node-expand-toggle"
-      >
-        <SvgImage className={classnames("w-full", "h-full")} />
-      </button>
-      <div className={classnames("px-2")} data-cy="research-node-item">
-        <div className={title}>{node.title}</div>
-        <p>{node.description}</p>
-        <table>
-          <tbody>
-            <tr>
-              <td className={classnames("px-2")}>
-                <span className={label}>Research time:</span>
-              </td>
-              <td className={classnames("px-2")}>
-                <span className={value}>
-                  {formatDurationShort(node.researchTime)}
-                </span>
-              </td>
-            </tr>
-            <tr>
-              <td className={classnames("px-2")}>
-                <span className={label}>Cost:</span>
-              </td>
-              <td className={classnames("px-2")}>
-                <span className={value}>1 genius flash</span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        {discovered && (
-          <section
-            className={classnames("m-4", "p-4", "bg-green-200")}
-            data-cy="research-node-already-researched"
-          >
-            <span>{node.title} has already been researched.</span>
-          </section>
-        )}
-        {isBeingResearched && (
-          <section
-            className={classnames("m-4", "p-4", "bg-green-200")}
-            data-cy="research-node-being-researched"
-          >
-            <span>This is currently being researched.</span>
-          </section>
-        )}
-        {!parentDiscovered && (
-          <section
-            className={classnames("m-4", "p-4", "bg-yellow-200")}
-            data-cy="research-node-parent-not-discovered"
-          >
-            <span>
-              You must research all previous items in this tree to unlock this
-              one.
-            </span>
-          </section>
-        )}
-        {showResearchButton && (
-          <form
-            className={classnames("my-2")}
-            onSubmit={(e) => {
-              e.preventDefault();
-              onSubmit();
-            }}
-          >
-            <button
-              type="submit"
-              className={classnames(styles.primaryButton)}
-              disabled={disabled}
-              data-cy="research-node-start-research-button"
-            >
-              Start Research
-            </button>
-          </form>
-        )}
-      </div>
-    </div>
-  );
-};
 function getAreaName(area: ResearchTree): string {
   switch (area) {
     case ResearchTree.DEMAND:
@@ -331,7 +84,9 @@ function GeniusFlashesSection() {
           Genius Flashes
         </span>
         <GeniusFlash className={"h-[3em] w-[3em]"} />
-        <span className="text-2xl" data-cy="available-genius-flashes">{geniusFlashes}</span>
+        <span className="text-2xl" data-cy="available-genius-flashes">
+          {geniusFlashes}
+        </span>
       </div>
 
       {currentCost && (
@@ -432,60 +187,69 @@ function ResearchSection() {
   return (
     <section>
       <h3>{getAreaName(area)}</h3>
-      <div className="w-[340px] h-[750px] mt-6 mx-auto relative">
-        <svg className="w-full h-full absolute">
-          {nodes.flatMap((r) => {
-            const req = r.requirements
-              .map((x) => research[x])
-              .filter((x) => x !== undefined);
-            const to = r;
-            const hasRequirements = Boolean(
-              to && to.requirements.every((x) => discoveries.includes(x))
-            );
-            return req.map((from) => (
-              <path
-                key={`${from.discovery}-${to.discovery}`}
-                d={`M${from.x + 50},${from.y + 50} L${to.x + 50},${to.y + 50}`}
-                className={classnames("stroke-[4]", {
-                  "stroke-green-600": hasRequirements,
-                  "stroke-gray-600": !hasRequirements,
-                })}
-              />
-            ));
-          })}
-        </svg>
-        {nodes.map((r) => {
-          const isResearched = discoveries.includes(r.discovery);
+      <div
+        className={classnames(
+          "origin-top scale-[85%] -translate-x-[8%] translate-y-0",
+          "xxs:scale-100 xxs:translate-x-0"
+        )}
+      >
+        <div className="w-[340px] h-[750px] mt-4 xs:mt-6 mx-auto relative ">
+          <svg className="w-full h-full absolute">
+            {nodes.flatMap((r) => {
+              const req = r.requirements
+                .map((x) => research[x])
+                .filter((x) => x !== undefined);
+              const to = r;
+              const hasRequirements = Boolean(
+                to && to.requirements.every((x) => discoveries.includes(x))
+              );
+              return req.map((from) => (
+                <path
+                  key={`${from.discovery}-${to.discovery}`}
+                  d={`M${from.x + 50},${from.y + 50} L${to.x + 50},${
+                    to.y + 50
+                  }`}
+                  className={classnames("stroke-[4]", {
+                    "stroke-green-600": hasRequirements,
+                    "stroke-gray-600": !hasRequirements,
+                  })}
+                />
+              ));
+            })}
+          </svg>
+          {nodes.map((r) => {
+            const isResearched = discoveries.includes(r.discovery);
 
-          return (
-            <button
-              key={r.discovery}
-              onClick={() => setSelected(r)}
-              className={classnames(
-                "w-[100px]",
-                "h-[100px]",
-                "rounded-lg",
-                "absolute",
-                "border",
-                "flex",
-                "text-center",
-                "justify-center",
-                "items-center",
-                "font-bold",
-                "p-2",
-                {
-                  "ring-4 ring-offset-4 ring-green-600": r === selected,
-                  "bg-green-50 text-black border-gray-400": !isResearched,
-                  "bg-green-600 text-white font-bold border-green-600": isResearched,
-                }
-              )}
-              style={{ transform: `translate3d(${r.x}px, ${r.y}px, 0)` }}
-              data-cy="research-node"
-            >
-              {r.title}
-            </button>
-          );
-        })}
+            return (
+              <button
+                key={r.discovery}
+                onClick={() => setSelected(r)}
+                className={classnames(
+                  "w-[100px]",
+                  "h-[100px]",
+                  "rounded-lg",
+                  "absolute",
+                  "border",
+                  "flex",
+                  "text-center",
+                  "justify-center",
+                  "items-center",
+                  "font-bold",
+                  "p-2",
+                  {
+                    "ring-4 ring-offset-4 ring-green-600": r === selected,
+                    "bg-green-50 text-black border-gray-400": !isResearched,
+                    "bg-green-600 text-white font-bold border-green-600": isResearched,
+                  }
+                )}
+                style={{ transform: `translate3d(${r.x}px, ${r.y}px, 0)` }}
+                data-cy="research-node"
+              >
+                {r.title}
+              </button>
+            );
+          })}
+        </div>
       </div>
       {selected !== null && (
         <div className="fixed bottom-0 left-0 right-0 p-4 bg-green-50 shadow-[0_0_16px_0_rgba(0,0,0,0.3)]">
@@ -585,7 +349,10 @@ function MainSection() {
         <GeniusFlash className={"h-[3em] w-[3em]"} />
         <span className="text-2xl">{geniusFlashes}</span>
         <Link to="genius-flashes">
-          <button className={classnames(primaryButton, "ml-8")} data-cy="get-more-button">
+          <button
+            className={classnames(primaryButton, "ml-8")}
+            data-cy="get-more-button"
+          >
             Get more
           </button>
         </Link>
